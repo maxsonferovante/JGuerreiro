c3f361aa8f3bcd683cde168175d84ff91151ea1d
Maxson Ferovante <maxsonferovante@gmail.com>
2016-04-29 14:23:44 -0300

    Correção no método DataAtual.

--- src/personagem/Data.java
+++ src/personagem/Data.java
@@ -37,19 +37,15 @@ public final class Data {
 	    return 1;
 	}
 	public static int[] DataAtual(){
-		
 		int datas[] = new int[3];
 		String tempData;
 		
-		Date date = new Date(System.currentTimeMillis());
-		
+		Date date = new Date(System.currentTimeMillis());	
 		tempData= date.toString();
-		
+	
 		datas[0] = Integer.parseInt(tempData.substring(0,4));
 		datas[1] = Integer.parseInt(tempData.substring(5,7));
-		datas[1] = Integer.parseInt(tempData.substring(8,10));
-		
-		System.out.println(datas[0]+datas[1]+datas[2]);
+		datas[2] = Integer.parseInt(tempData.substring(8,10));
 		
 		return datas;
 	}
