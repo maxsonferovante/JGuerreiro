a4e0eedd64878f97f43a1801e42a0eda074ea8c1
Maxson Ferovante <maxsonferovante@gmail.com>
2016-04-29 13:58:01 -0300

    Crianção do método static DataAtual na classe Data. Ele retorna um vetor de inteiros com três posições. Na posião 0: dia. Na posião 1: mes. Na posião 2: ano.

--- src/personagem/Data.java
+++ src/personagem/Data.java
@@ -1,5 +1,7 @@
 package personagem;
 
+import java.sql.Date;
+
 public final class Data {
 	private int day, month, year;
 	
@@ -33,5 +35,22 @@ public final class Data {
 	    if (this.month == 2 && dia == 29 && (this.year%400==0 || (this.year%4 == 0 && this.year %100 != 0)))
 	        return dia;
 	    return 1;
+	}
+	public static int[] DataAtual(){
+		
+		int datas[] = new int[3];
+		String tempData;
+		
+		Date date = new Date(System.currentTimeMillis());
+		
+		tempData= date.toString();
+		
+		datas[0] = Integer.parseInt(tempData.substring(0,4));
+		datas[1] = Integer.parseInt(tempData.substring(5,7));
+		datas[1] = Integer.parseInt(tempData.substring(8,10));
+		
+		System.out.println(datas[0]+datas[1]+datas[2]);
+		
+		return datas;
 	}
 }
